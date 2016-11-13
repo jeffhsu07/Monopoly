7f794456aa04fed35ad6f6f4119d70e39b8ed0a5
Matthew van Niekerk <vannieke@usc.edu>
2016-11-13 15:16:46 -0800

    Added AppearanceSettings

--- Monopoly/src/client/LoginWindow.java
+++ Monopoly/src/client/LoginWindow.java
@@ -1,5 +1,48 @@
 package client;
 
-public class LoginWindow {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
+public class LoginWindow extends JFrame {
+	
+	private JPanel mainPanel;
+	private JLabel alertLabel;
+	private JButton loginButton;
+	private JButton createAccountButton;
+	private JTextField usernameTF;
+	private JPasswordField passwordField;
+	
+	public LoginWindow() {
+		setSize(600, 600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		initializeComponents();
+		createGUI();
+		addListeners();
+	}
+	
+	private void initializeComponents() {
+		mainPanel = new JPanel();
+		alertLabel =new JLabel("");
+		loginButton = new JButton("Log In");
+		createAccountButton = new JButton("Create Account");
+		usernameTF = new JTextField("Username");
+		passwordField = new JPasswordField("Password");
+		passwordField.setEchoChar((char) 0);
+	}
+	
+	private void createGUI() {
+		
+	}
+	
+	private void addListeners() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
